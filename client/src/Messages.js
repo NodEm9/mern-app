4de69586db4de4b1362e777db51935afe1a72832
NodEm9 <emma0martins@gmail.com>
2024-11-07 15:59:44 +0000

    Create axios instance to make url less verbose

--- client/src/Messages.js
+++ client/src/Messages.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
-import axios from 'axios'
 import formattedDate from './utils/formatDate'
 import messageSound from './assets/sounds/tap-notification.mp3'
+import logo from './assets/img/chat-logo.svg'
+import instance from './utils/config/axiosConfig'
 
-function Messages() {
+const axios = instance
+
+const Messages = () => {
   const [messages, setMessages] = useState([])
   const [message, setMessage] = useState('')
   const [err, setErr] = useState('')
   const [success, setSuccess] = useState('')
-
   const [createdAt, setCreatedAt] = useState('')
   const [isFocused, setIsFocused] = useState(false)
 
@@ -20,7 +22,7 @@ function Messages() {
 
   // Fetch messages from the server
   const fetchMessages = async () => {
-    const response = await axios.get('http://172.104.246.147:5000/messages')
+    const response = await axios.get('/messages');
     if (!response.data) return setMessages(['No messages']);
     const date = await response.data[0]?.createdAt;
     setCreatedAt(date);
@@ -33,19 +35,16 @@ function Messages() {
     if (message === '') {
       setErr('You must enter a message');
     } else {
-      const response = await axios.post('http://172.104.246.147:5000/messages',
+      await axios.post('/messages',
         {
           messageText: message
         });
-      
-      if (!response.data) setErr(response.data?.error);
 
       // Play a sound when the message is sent
-        const audion = new Audio(messageSound);
-        audion.play();
-        setSuccess('Message sent ✅');
+      const audion = new Audio(messageSound);
+      audion.play();
+      setSuccess('Message sent ✅');
 
-  
       fetchMessages();
       setMessage('');
       if (isFocused) setErr('');
@@ -58,6 +57,10 @@ function Messages() {
       <div ref={errRef}>{err && err ? <p className='error'>{err}</p> : null}</div>
       <div>{success ? <p className='success'>{success}</p> : null}</div>
       <form onSubmit={sendMessage}>
+        <div className='form-header'>
+          <img src={logo} alt='chat logo' />
+          <h2>Peep 2 Peep</h2>
+        </div>
         <fieldset>
           <legend htmlFor='message'>Message</legend>
           <input
