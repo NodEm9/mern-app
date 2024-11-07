beede94879425f57ab62b17cf11b19c790dcf456
NodEm9 <emma0martins@gmail.com>
2024-11-07 14:03:18 +0000

    Integrate customer error to handle errors

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const PORT = 5000;
 import cors from 'cors';
 import corsOptions from './config/corsOptions.js';
 import credentials from './middleware/credentials.js';
+import { handler } from './middleware/errorHandler.js';
+
 
 const app = express();
 import db from './dbConfig.js';
@@ -21,54 +23,34 @@ app.use(express.urlencoded({ extended: true }));
 // GET request to get all messages
 app.get('/messages', (req, res, next) => {
   MessageModel
-    .find({}, 'messageText')
-    .then(data => res.json(data))
+    .find({}, 'messageText createdAt')
+    .then(data => {
+      if (!data) {
+        return res.status(404).json({ message: 'No messages found' });
+      }
+      res.json(data);
+    })
     .catch(next);
 });
 
 // POST request to create a new message
-app.post('/messages', (req, res, next) => {
+app.post('/messages', async (req, res, next) => {
   if (req.body.messageText) {
     MessageModel.create(req.body)
       .then(data => res.json(data))
       .catch(next);
   } else {
-    res.json(errorHandler('Message text is required'));
+    console.log(await handler.handleError(req, 400));
+    res.json(await handler.handleError(req, 400));
   }
 });
 
 // Error handling middleware
-function errorHandler(err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).json({ error: 'The user is not authorized' });
-    return next(err)
-  }
-
-  if (err.name === 'ValidationError') {
-    res.status(422).json({ error: err.message });
-    return next(err)
-  }
-
-  if (err.name === 'CastError') {
-    res.status(400).json({ error: 'Invalid ID' });
-    return next(err)
-  }
-  
-  if (err.name === 'MongoError') {
-    res.status(409).json({ error: 'Duplicate key' });
-    return next(err)
-  }
-
-  process.exit(1);
-  res.status(500).json({ error: err });
-}
-
-// Error handling middleware
-app.use(errorHandler);
+app.use(handler.handleError);
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
- 
+
 export default app;
