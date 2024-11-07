b84c2f928bad2303eb1536a974cb4a4219e64344
NodEm9 <emma0martins@gmail.com>
2024-11-07 13:58:38 +0000

    Add allowed origins for cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,7 @@
 const allowedOrigins = [
   'http://172.104.246.147:3000',
-  "http://localhost:3000",
-  "http@//172.104.246.147:5001",
-  "htpp://172.104.246.147:5000"
+  "http@//172.104.246.147:5000",
+  "htpp://172.104.246.147:5001"
 ];
 
 export default allowedOrigins;
