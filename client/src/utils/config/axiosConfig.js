237f35b0749d2e496750f5794a90eb61027b8d9c
NodEm9 <emma0martins@gmail.com>
2024-11-08 14:23:51 +0000

    Move axios config file and directory to a different path

--- client/src/utils/config/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: 'http://172.104.246.147:5000/',
-  timeout: 1000,
-  headers: {'Content-Type': 'application/json'}
-});
-
-export default instance;
